2068f929afd34493b9badf9ee4eb1d04a78f58af
Cristian Macovei <118800887+cristianmacovei@users.noreply.github.com>
2023-06-20 14:24:12 +0300

    SearchBar
    Functionality for searchbar. Input filters the API URL for set value and displays the resulting json file.

--- assets/js/example.js
+++ assets/js/example.js
@@ -1,4 +1,26 @@
-let source = "https://openlibrary.org/search/authors.json?q=maxwell";
-fetch(source)
-  .then((x) => x.text())
-  .then((y) => (document.getElementById("demo").innerHTML = y));
+// Function to store and print the input value of the search box
+function handleKey(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    var inputElement = document.getElementById("inlineFormInputGroupUsername");
+    var val = inputElement.value;
+    console.log(val);
+
+    handleAPIOnInputValue(val);
+  }
+}
+// Call the function
+var inputElement = document.getElementById("inlineFormInputGroupUsername");
+inputElement.addEventListener("keydown", handleKey);
+
+// Function to Handle API based on the search box input
+function handleAPIOnInputValue(input) {
+  // Store the search value and replace SPACES with "+" to pass into the API URL.
+  let searchValue = input.replace(" ", "+");
+
+  // Pass the searchValue into the API URL and display search results.
+  let source = `https://openlibrary.org/search.json?q=${searchValue}`;
+  fetch(source)
+    .then((x) => x.text())
+    .then((y) => (document.getElementById("demo").innerHTML = y));
+}
