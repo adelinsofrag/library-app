a19e79f1dcca98ec933d938458a9dc9a2853c11c
adelinsofrag <adelin.sofrag@gmail.com>
2023-06-21 20:14:25 +0300

    am adaugat handleData fn - compune si insereaza un html
    am implementat o componenta - drawIMG, care foloseste covers.openlibrary pentru a afisa copertele.

--- assets/js/example.js
+++ assets/js/example.js
@@ -1,4 +1,30 @@
-let source = "https://openlibrary.org/search/authors.json?q=maxwell";
+// let source = "https://openlibrary.org/search/authors.json?q=maxwell";
+
+let source = "https://openlibrary.org/search.json?q=rowling&mode=everything";
 fetch(source)
-  .then((x) => x.text())
-  .then((y) => (document.getElementById("demo").innerHTML = y));
+  .then((x) => x.json())
+  .then((y) => handleData(y.docs));
+
+
+function handleData(data) {
+  let html = "";
+
+  data.forEach((element) => {
+    html += `
+        <div class="card" style="width: 18rem;">
+          ${drawIMG(element.cover_i)}
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      `;
+  });
+
+  return (document.getElementById("demo").innerHTML = html);
+}
+
+function drawIMG(input) {
+  return `<img class="card-img-top" src="https://covers.openlibrary.org/b/id/${input}-M.jpg">`;
+}
